1d310cb4f3e49a57f5a534e57d573f20670d1e10
Mauricio Barroso Benavides <mauriciobarroso1990@gmail.com>
2025-06-26 22:13:01 -0400

    added function to register a callback function

--- include/tpl5010.h
+++ include/tpl5010.h
@@ -1,36 +1,36 @@
 /**
-  ******************************************************************************
-  * @file           : tpl5010.h
-  * @author         : Mauricio Barroso Benavides
-  * @date           : Jul 17, 2023
-  * @brief          : todo: write brief 
-  ******************************************************************************
-  * @attention
-  *
-  * MIT License
-  *
-  * Copyright (c) 2023 Mauricio Barroso Benavides
-  *
-  * Permission is hereby granted, free of charge, to any person obtaining a copy
-  * of this software and associated documentation files (the "Software"), to
-  * deal in the Software without restriction, including without limitation the
-  * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
-  * sell copies of the Software, and to permit persons to whom the Software is
-  * furnished to do so, subject to the following conditions:
-  *
-  * The above copyright notice and this permission notice shall be included in
-  * all copies or substantial portions of the Software.
-  * 
-  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-  * IN THE SOFTWARE.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : tpl5010.h
+ * @author         : Mauricio Barroso Benavides
+ * @date           : Jul 17, 2023
+ * @brief          : todo: write brief
+ ******************************************************************************
+ * @attention
+ *
+ * MIT License
+ *
+ * Copyright (c) 2023 Mauricio Barroso Benavides
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ ******************************************************************************
+ */
 
 /* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef TPL5010_H_
@@ -41,44 +41,96 @@ extern "C" {
 #endif
 
 /* Includes ------------------------------------------------------------------*/
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
-#include "freertos/FreeRTOS.h"
+#if CONFIG_IDF_TARGET_ESP32 || CONFIG_IDF_TARGET_ESP32S2 ||                    \
+    CONFIG_IDF_TARGET_ESP32S3 || CONFIG_IDF_TARGET_ESP32C2 ||                  \
+    CONFIG_IDF_TARGET_ESP32C3 || CONFIG_IDF_TARGET_ESP32C6 ||                  \
+    CONFIG_IDF_TARGET_ESP32P4
+#define ESP32_TARGET
+#endif
+
+#ifdef ESP32_TARGET
 #include "driver/gpio.h"
+#else
+#include "main.h"
+#endif /* ESP32_TARGET */
 
 /* Exported Macros -----------------------------------------------------------*/
 
 /* Exported typedef ----------------------------------------------------------*/
+/**
+ * @brief Millisecond delay function prototype.
+ * @param ms : Delay time in milliseconds.
+ */
+typedef void (*tpl5010_delay_t)(uint32_t ms);
+
+/**
+ * @brief Callback function prototype invoked on WAKE pulse.
+ * @param arg: User data pointer provided at registration.
+ */
+typedef void (*tpl5010_cb_t)(void *arg);
+
+/**
+ * @brief Driver configuration structure for the TPL5010 system wake-up timer.
+ */
 typedef struct {
-	int wake_gpio;
-	int done_gpio;
+  int wake_gpio; /*!< GPIO connected to TPL5010 WAKE output pin; pulses high
+                    every tIP period to wake the MCU */
+  int done_gpio; /*!< GPIO connected to TPL5010 DONE input pin; MCU must assert
+                    a low-to-high transition to reset the watchdog counter. */
+  tpl5010_delay_t delay_ms; /*!< User-supplied function to delay execution by
+                               `ms` milliseconds. */
+  tpl5010_cb_t cb_fn;       /*!< User callback invoked upon each WAKE pulse. */
+  void *cb_arg; /*!< User-defined context pointer passed to `cb_fn`. */
 } tpl5010_t;
 
 /* Exported variables --------------------------------------------------------*/
 
 /* Exported functions prototypes ---------------------------------------------*/
 /**
- * @brief Function to write a byte
+ * @brief Initialize the TPL5010 driver instance.
  *
- * @param me        : Pointer to a structure instance of at24cs0x_t
- * @param data_addr : Address to write data
- * @param data      : Data to write
+ * @param me[in]        : Pointer to a tpl5010_t structure (must be initialized
+ * before use)
+ * @param wake_gpio[in] : MCU GPIO pin number connected to the TPL5010 WAKE
+ * output; configure this pin as an input to detect the 20 ms WAKE pulse
+ * @param done_gpio[in] : MCU GPIO pin number connected to the TPL5010 DONE
+ * input; configure this pin as an output to issue the low-to-high DONE
+ * transition that resets the watchdog counter
  *
- * @return ESP_OK on success
+ * @return 0 on success, -1 on failure
  */
-esp_err_t tpl5010_init(tpl5010_t *const me, int wake_gpio, int done_gpio);
+int tpl5010_init(tpl5010_t *const me, int wake_gpio, int done_gpio);
 
 /**
- * @brief Function to write a byte
+ * @brief Register a user callback to be invoked on each WAKE pulse.
  *
- * @param me        : Pointer to a structure instance of at24cs0x_t
- * @param data_addr : Address to write data
- * @param data      : Data to write
+ * @param me[in]     : Pointer to a tpl5010_t structure previously initialized
+ * with tpl5010_init()
+ * @param cb_fn[in]  : Function pointer to call when a WAKE pulse is detected;
+ * signature must be `void (*callback)(void *arg)`
+ * @param cb_arg[in] : User-defined context pointer passed to the callback when
+ * invoked
  *
- * @return ESP_OK on success
+ * @return 0 on success, -1 on failure
  */
-esp_err_t tpl5010_done(tpl5010_t *const me);
+int tpl5010_register_callback(tpl5010_t *const me, tpl5010_cb_t cb_fn,
+                              void *cb_arg);
+
+/**
+ * @brief Issue the DONE signal to the TPL5010, indicating successful processing
+ * of the last WAKE pulse.
+ *
+ * @param me[in] : Pointer to a tpl5010_t structure previously initialized with
+ * tpl5010_init()
+ *
+ * @return 0 on success, -1 on failure
+ */
+int tpl5010_done(tpl5010_t *const me);
 
 #ifdef __cplusplus
 }
--- tpl5010.c
+++ tpl5010.c
@@ -1,46 +1,50 @@
 /**
-  ******************************************************************************
-  * @file           : tpl5010.c
-  * @author         : Mauricio Barroso Benavides
-  * @date           : Jul 17, 2023
-  * @brief          : todo: write brief
-  ******************************************************************************
-  * @attention
-  *
-  * MIT License
-  *
-  * Copyright (c) 2023 Mauricio Barroso Benavides
-  *
-  * Permission is hereby granted, free of charge, to any person obtaining a copy
-  * of this software and associated documentation files (the "Software"), to
-  * deal in the Software without restriction, including without limitation the
-  * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
-  * sell copies of the Software, and to permit persons to whom the Software is
-  * furnished to do so, subject to the following conditions:
-  *
-  * The above copyright notice and this permission notice shall be included in
-  * all copies or substantial portions of the Software.
-  * 
-  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-  * IN THE SOFTWARE.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : tpl5010.c
+ * @author         : Mauricio Barroso Benavides
+ * @date           : Jul 17, 2023
+ * @brief          : todo: write brief
+ ******************************************************************************
+ * @attention
+ *
+ * MIT License
+ *
+ * Copyright (c) 2023 Mauricio Barroso Benavides
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ ******************************************************************************
+ */
 
 /* Includes ------------------------------------------------------------------*/
 #include "tpl5010.h"
 
+#ifdef ESP32_TARGET
 #include "driver/gpio.h"
-#include "esp_timer.h"
+#include "esp_err.h"
 #include "esp_log.h"
+#else
+/* todo: write for STM32 devices */
+#endif /* ESP32_TARGET */
 
 /* Private macros ------------------------------------------------------------*/
-#define NOP() asm volatile ("nop")
+#define NOP() asm volatile("nop")
 
 /* External variables --------------------------------------------------------*/
 
@@ -50,151 +54,135 @@
 static const char *TAG = "tpl5010";
 
 /* Private function prototypes -----------------------------------------------*/
-/**
- * @brief Function to write a byte
- *
- * @param me        : Pointer to a structure instance of at24cs0x_t
- * @param data_addr : Address to write data
- * @param data      : Data to write
- *
- * @return ESP_OK on success
- */
-static void delay_us(uint32_t period_us);
-
-/**
- * @brief Function to write a byte
- *
- * @param me        : Pointer to a structure instance of at24cs0x_t
- * @param data_addr : Address to write data
- * @param data      : Data to write
- *
- * @return ESP_OK on success
- */
-static void IRAM_ATTR gpio_isr_handler(void *arg);
+static int set_done_level(int done_gpio, bool level);
 
 /* Exported functions definitions --------------------------------------------*/
 /**
- * @brief Function to write a byte
+ * @brief Initialize the TPL5010 driver instance.
  */
-esp_err_t tpl5010_init(tpl5010_t *const me, int wake_gpio, int done_gpio) {
-	esp_err_t ret = ESP_OK;
+int tpl5010_init(tpl5010_t *const me, int wake_gpio, int done_gpio) {
+  int ret = 0;
 
-	/* Configure wake GPIO */
-	me->wake_gpio = wake_gpio;
+  /* Configure wake GPIO */
+  me->wake_gpio = wake_gpio;
 
-	gpio_config_t gpio_conf;
-	gpio_conf.intr_type = GPIO_INTR_POSEDGE;
-	gpio_conf.mode = GPIO_MODE_INPUT;
-	gpio_conf.pin_bit_mask = 1ULL << me->wake_gpio;
-	gpio_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
-	gpio_conf.pull_up_en = GPIO_PULLUP_ENABLE;
+#ifdef ESP32_TARGET
+  gpio_config_t gpio_conf;
+  gpio_conf.intr_type = GPIO_INTR_POSEDGE;
+  gpio_conf.mode = GPIO_MODE_INPUT;
+  gpio_conf.pin_bit_mask = 1ULL << me->wake_gpio;
+  gpio_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
+  gpio_conf.pull_up_en = GPIO_PULLUP_ENABLE;
+  ret = gpio_config(&gpio_conf);
 
-	ret = gpio_config(&gpio_conf);
+  if (ret != ESP_OK) {
+    ESP_LOGE(TAG, "Failed to configure wake GPIO");
+    return ret;
+  }
+#else
+  /* todo: write for STM32 devices */
+#endif /* ESP32_TARGET */
 
-	if (ret != ESP_OK) {
-		ESP_LOGE(TAG, "Failed to configure wake GPIO");
-		return ret;
-	}
+  /* Configure done GPIO */
+  me->done_gpio = done_gpio;
 
-	/* Configure done GPIO */
-	me->done_gpio = done_gpio;
+#ifdef ESP32_TARGET
+  gpio_conf.intr_type = GPIO_INTR_DISABLE;
+  gpio_conf.mode = GPIO_MODE_OUTPUT;
+  gpio_conf.pin_bit_mask = 1ULL << me->done_gpio;
+  gpio_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
+  gpio_conf.pull_up_en = GPIO_PULLUP_DISABLE;
+  ret = gpio_config(&gpio_conf);
 
-	gpio_conf.intr_type = GPIO_INTR_DISABLE;
-	gpio_conf.mode = GPIO_MODE_OUTPUT;
-	gpio_conf.pin_bit_mask = 1ULL << me->done_gpio;
-	gpio_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
-	gpio_conf.pull_up_en = GPIO_PULLUP_DISABLE;
+  if (ret != ESP_OK) {
+    ESP_LOGE(TAG, "Failed to configure done GPIO");
+    return ret;
+  }
+#else
+  /* todo: write for STM32 devices */
+#endif /* ESP32_TARGET */
 
-	ret = gpio_config(&gpio_conf);
+  /* Install GPIO ISR service */
+  ret = gpio_install_isr_service(0);
 
-	if (ret != ESP_OK) {
-		ESP_LOGE(TAG, "Failed to configure done GPIO");
-		return ret;
-	}
+  if (ret != ESP_OK && ret != ESP_ERR_INVALID_STATE) {
+    ESP_LOGE(TAG, "Failed to install wake GPIO ISR service");
+    return ret;
+  }
 
-	/* Install GPIO ISR service */
-	ret = gpio_install_isr_service(0);
+  /* Send done signal */
+  tpl5010_done(me);
 
-	if (ret != ESP_OK && ret != ESP_ERR_INVALID_STATE) {
-		ESP_LOGE(TAG, "Failed to install wake GPIO ISR service");
-		return ret;
-	}
+  /* Return success */
+  return ret;
+}
 
-	/* Add GPIO ISR handler */
-	ret = gpio_isr_handler_add(me->wake_gpio, gpio_isr_handler, me);
+/**
+ * @brief Register a user callback to be invoked on each WAKE pulse.
+ */
+int tpl5010_register_callback(tpl5010_t *const me, tpl5010_cb_t cb_fn,
+                              void *cb_arg) {
+  int ret = 0;
 
-	if (ret != ESP_OK) {
-		ESP_LOGE(TAG, "Failed to add GPIO ISR handler");
-		return ret;
-	}
+  /* Check if the callback functions is valid */
+  if (cb_fn == NULL) {
+    return -1;
+  }
 
-	ret = tpl5010_done(me);
+  /* Assign callback function and argument to structure members */
+  me->cb_fn = cb_fn;
+  me->cb_arg = cb_arg;
 
-	if (ret != ESP_OK) {
-		return ret;
-	}
+/* Add GPIO ISR handler */
+#ifdef ESP32_TARGET
+  ret = gpio_isr_handler_add(me->wake_gpio, me->cb_fn, me->cb_arg);
 
-	/* Return ESP_OK */
-	return ret;
+  if (ret != ESP_OK) {
+    ESP_LOGE(TAG, "Failed to add GPIO ISR handler");
+    return ret;
+  }
+#else
+  /* todo: write for STM32 devices */
+#endif /* ESP32_TARGET */
+
+  /* Return success */
+  return ret;
 }
 
 /**
  * @brief Function to write a byte
  */
 esp_err_t tpl5010_done(tpl5010_t *const me) {
-	esp_err_t ret = ESP_OK;
+  esp_err_t ret = ESP_OK;
 
-	/* Send an initial pulse to TPL5010 */
-	ret = gpio_set_level(me->done_gpio, true);
+  /* Put in high level the done GPIO */
+  ret = set_done_level(me->done_gpio, 1);
 
-	if (ret != ESP_OK) {
-		ESP_LOGE(TAG, "Failed to set done GPIO level");
-		return ret;
-	}
+  if (ret != 0) {
+    return ret;
+  }
 
-	delay_us(100);
+  /* Wait for 100 ms */
+  me->delay_ms(100);
 
-	ret = gpio_set_level(me->done_gpio, false);
+  /* Put in low level the done GPIO */
+  ret = set_done_level(me->done_gpio, 0);
 
-	if (ret != ESP_OK) {
-		ESP_LOGE(TAG, "Failed to set done GPIO level");
-		return ret;
-	}
+  if (ret != 0) {
+    return ret;
+  }
 
-	/* Return ESP_OK */
-	return ret;
+  /* Return ESP_OK */
+  return ret;
 }
 
 /* Private function definitions ----------------------------------------------*/
-/**
- * @brief Function to write a byte
- */
-static void delay_us(uint32_t period_us) {
-	uint64_t m = (uint64_t)esp_timer_get_time();
-
-  if (period_us) {
-  	uint64_t e = (m + period_us);
-
-  	if (m > e) { /* overflow */
-  		while ((uint64_t)esp_timer_get_time() > e) {
-  			NOP();
-  		}
-  	}
-
-  	while ((uint64_t)esp_timer_get_time() < e) {
-  		NOP();
-  	}
-  }
-}
-
-/**
- * @brief Function to write a byte
- */
-static void IRAM_ATTR gpio_isr_handler(void *arg) {
-	tpl5010_t *tpl5010 = (tpl5010_t *)arg;
-
-	ESP_DRAM_LOGI(TAG, "Wake");
-	tpl5010_done(tpl5010);
+static int set_done_level(int done_gpio, bool level) {
+#ifdef ESP32_TARGET
+  return gpio_set_level(done_gpio, level);
+#else
+  /* todo: write for STM32 devices */
+#endif /* ESP32_TARGET */
 }
 
 /***************************** END OF FILE ************************************/
